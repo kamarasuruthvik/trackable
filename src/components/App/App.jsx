55f96c8392944d577e2ef5d81e87920d9a3bbec8
kamarasuruthvik <kamarasuruthvik@gmail.com>
2021-06-27 15:51:34 +0530

    Added Nav component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import './App.css';
+const App = ()=>{
+    return(
+        <div className="app-container">
+            <Nav/>
+        </div>
+        
+        );
+
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = ()=>{
+    return(
+        <div className="nav-container">
+            <div className="nav-header">
+                <p>
+                    Trackable
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
