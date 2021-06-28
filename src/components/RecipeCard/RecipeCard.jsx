51ec90deefb652e25628d0bb1795471f62f0476e
kamarasuruthvik <kamarasuruthvik@gmail.com>
2021-06-28 07:33:38 +0530

    Added flex wrap on results container

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -3,14 +3,13 @@ import './RecipeCard.css';
 const RecipeCard = ({recipe})=>{
     return(
         <div className="recipe-card">
-            <div className="recipe-image">
-                <img src={recipe.recipe.image} alt="recipe-image"/>
-            </div>
+            <img className="recipe-image" src={recipe.recipe.image} alt="recipe-image"/>
+
             <div className="recipe-name">
                 <p>{recipe.recipe.label}</p>
             </div>
             <div className="recipe-add-button">
-                <button>
+                <button className="add-recipe">
                     Add
                 </button>
             </div>
