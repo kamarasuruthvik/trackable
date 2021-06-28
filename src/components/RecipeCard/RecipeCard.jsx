a4f4046832ac7402801a199e4d05818cbf523208
kamarasuruthvik <kamarasuruthvik@gmail.com>
2021-06-28 14:22:08 +0530

    Added style for cards and transitions

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -7,14 +7,16 @@ const RecipeCard = ({recipe})=>{
 
             <div className="recipe-name">
                 <p className="name">{((recipe.recipe.label.length>12)?(recipe.recipe.label.slice(0,10)+ '...'):
-                (recipe.recipe.label)).toUpperCase() }</p>
+                (recipe.recipe.label)) }</p>
             </div>
             <div className="recipe-add-button">
                 <button className="add-recipe">
-                    + {recipe.recipe.calories}
+                    + {Math.floor(recipe.recipe.calories) + ' cal'}
                 </button>
             </div>
-
+            <div className="view-more-container">
+                <p>VIEW MORE</p>
+            </div>
         </div>
     );
 }
